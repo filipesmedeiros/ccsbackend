71d34502a4eae0e2b703b07aa8c7a8117fb62447
Filipe Medeiros <filipesilvamedeiros@gmail.com>
2019-10-13 23:17:30 +0100

    ijawid

--- src/utils/Secrets.java
+++
@@ -1,6 +0,0 @@
-package utils;
-
-public class Secrets {
-
-    public static final String AZURE_STORAGE_KEY = "DefaultEndpointsProtocol=https;AccountName=ccsbackend47967;AccountKey=ojJM4HhOV+OoDIZJRa+/jrOJup+5QVOYrY6HLrWBKYD7kF+AuIqEQvR6jxxFA7Ksq2S3mnJFBjTS3yrki8ABPg==;EndpointSuffix=core.windows.net";
-}
