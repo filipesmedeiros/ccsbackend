f0ef5bf5b9bba363bd8181c2cea222299645c2e2
Filipe Medeiros <filipesilvamedeiros@gmail.com>
2019-10-13 22:57:10 +0100

    Stuff

--- src/utils/Secrets.java
+++ src/utils/Secrets.java
@@ -2,5 +2,5 @@ package utils;
 
 public class Secrets {
 
-    public static final String AZURE_STORAGE_KEY = "DefaultEndpointsProtocol=https;AccountName=ccsbackend47967;AccountKey=EVZmpP8dslJOnKXanpz2pB1fiqEindwJf3nr7kvBWOpcNMbsFQdHdkLLqXRoXjKcDQOfnFVecaQ9yC4iH6UOnA==;EndpointSuffix=core.windows.net";
+    public static final String AZURE_STORAGE_KEY = "DefaultEndpointsProtocol=https;AccountName=ccsbackend47967;AccountKey=;EndpointSuffix=core.windows.net";
 }
