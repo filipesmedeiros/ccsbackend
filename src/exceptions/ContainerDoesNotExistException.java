2ade8f1706989e9be92b1d97b23f729c9d0677a7
Filipe Medeiros <filipesilvamedeiros@gmail.com>
2019-10-13 23:05:22 +0100

    lmaooo

--- src/exceptions/ContainerDoesNotExistException.java
+++
@@ -1,8 +0,0 @@
-package exceptions;
-
-public class ContainerDoesNotExistException extends Exception {
-
-    public ContainerDoesNotExistException() {
-        super("The container doesn't exist.");
-    }
-}
--- src/utils/Secrets.java
+++
@@ -1,6 +0,0 @@
-package utils;
-
-public class Secrets {
-
-    public static final String AZURE_STORAGE_KEY = "DefaultEndpointsProtocol=https;AccountName=ccsbackend47967;AccountKey=;EndpointSuffix=core.windows.net";
-}
