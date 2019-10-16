f22161954b76bd88c888c1469ca7859c0b6073ed
Filipe Medeiros <filipesilvamedeiros@gmail.com>
2019-10-16 14:32:47 +0100

    List of TODOs

--- src/scc/srv/MainApplication.java
+++ src/scc/srv/MainApplication.java
@@ -1,7 +1,5 @@
 package scc.srv;
 
-import resources.Subreddit;
-
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 import java.util.HashSet;
@@ -9,6 +7,17 @@ import java.util.Set;
 
 @ApplicationPath("/")
 public class MainApplication extends Application {
+
+    // TODO fazer verificacoes quando vamos fazer posts (user existe, etc)
+    // TODO verificar se estamos a tratar bem das excecoes que podem aparecer a aceder ao cosmos
+    // TODO fazer sistema de likes e dislikes
+    // TODO fazer os comentarios
+    // TODO decidir e implementar o sistema de frontpage (all)
+    // TODO decidir e implementar o sistema de frontpage (subreddit)
+    // TODO decidir e implementar os comentários de um post
+
+    // TODO ------ CENAS OPCIONAIS MAS QUE DEVEM SER FIXES E NAO MUITO DIFICEIS ----
+    // TODO sistema de subscricao a subreddits, para um user ter a sua propria frontpage
 
     @Override
     public Set<Class<?>> getClasses() {
